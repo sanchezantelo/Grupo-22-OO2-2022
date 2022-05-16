213ef4dcdafd09631a60d87db8faeaa74bd25706
Sofia Sanchez Antelo <sofia.sanchez@claro.com.ar>
2022-05-16 14:02:18 -0300

    Se modifica script de la base de datos

--- grupo-22-bdd-oo2-2022.sql
+++ grupo-22-bdd-oo2-2022.sql
@@ -1,6 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `grupo-22-bdd-oo2-2022` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE SCHEMA IF NOT EXISTS `grupo-22-bdd-oo2-2022` DEFAULT CHARACTER SET utf8 ;
 USE `grupo-22-bdd-oo2-2022`;
-
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: localhost    Database: grupo-22-bdd-oo2-2022
@@ -19,28 +18,73 @@ USE `grupo-22-bdd-oo2-2022`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `persona`
+-- Table structure for table `usuario`
 --
 
-DROP TABLE IF EXISTS `persona`;
+DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `persona` (
-  `idPersona` int NOT NULL,
-  PRIMARY KEY (`idPersona`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `usuario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `apellido` varchar(45) NOT NULL,
+  `clave` varchar(60) NOT NULL,
+  `createdat` datetime(6) DEFAULT NULL,
+  `dni` bigint NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `enabled` tinyint(1) DEFAULT '1',
+  `nombre` varchar(45) NOT NULL,
+  `tipo_documento` varchar(255) NOT NULL,
+  `updatedat` datetime(6) NOT NULL,
+  `usuario` varchar(45) NOT NULL,
+  `rol_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_ma71x4n4tydibsd9qt0m71le7` (`dni`),
+  UNIQUE KEY `UK_5171l57faosmj8myawaucatdw` (`email`),
+  UNIQUE KEY `UK_i02kr8ui5pqddyd7pkm3v4jbt` (`usuario`),
+  KEY `FKbl9l2tfkiy0krho63kbqmlwyf` (`rol_id`),
+  CONSTRAINT `FKbl9l2tfkiy0krho63kbqmlwyf` FOREIGN KEY (`rol_id`) REFERENCES `usuario_rol` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `persona`
+-- Dumping data for table `usuario`
 --
 
-LOCK TABLES `persona` WRITE;
-/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
-/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'admin','21232f297a57a5a743894a0e4a801fc3','2022-05-16 12:05:07.317713',11111111,'admin@admin.com',1,'admin','DNI','2022-05-16 12:05:07.317713','admin',1),(2,'auditor','f7d07071ed9431ecae3a8d45b4c82bb2','2022-05-16 12:03:04.781961',22222222,'auditor@auditor.com',1,'auditor','DNI','2022-05-16 12:03:04.781961','auditor',2);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+--
+-- Table structure for table `usuario_rol`
+--
+
+DROP TABLE IF EXISTS `usuario_rol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario_rol` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `createdat` datetime(6) DEFAULT NULL,
+  `enabled` tinyint(1) DEFAULT '1',
+  `rol` varchar(100) NOT NULL,
+  `updatedat` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_9fb2oynawbjqah9pylnfgkxfj` (`rol`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario_rol`
+--
+
+LOCK TABLES `usuario_rol` WRITE;
+/*!40000 ALTER TABLE `usuario_rol` DISABLE KEYS */;
+INSERT INTO `usuario_rol` VALUES (1,'2022-05-16 12:05:07.317713',1,'ROL_ADMIN','2022-05-16 12:28:10.214720'),(2,'2022-05-16 12:05:07.317713',1,'ROL_AUDITOR','2022-05-16 11:34:23.423287');
+/*!40000 ALTER TABLE `usuario_rol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -49,4 +93,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-05 15:36:04
+-- Dump completed on 2022-05-16 13:01:23
