4e32ffa1275fa39170e7ba0d0600e0a527008ae4
lesrz <suarezarnaldi.leo@gmail.com>
2022-05-20 15:33:15 -0300

    Crear modelo Curso

---
+++ src/main/java/com/webservice/app/models/CursoModel.java
@@ -0,0 +1,37 @@
+package com.webservice.app.models;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.webservice.app.entities.Usuario;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class CursoModel extends NotaPedidoModel {
+	protected int codigo;
+	protected char turno;
+	@DateTimeFormat(pattern= "yyyy-MM-dd")
+	protected LocalDate fechaInicio;
+	@DateTimeFormat(pattern= "yyyy-MM-dd")
+	protected LocalDate fechaFin;
+	protected String cuatrimestre;
+	protected String presencialidad;	
+	
+	public CursoModel(int id, LocalDate fecha, int cantEstudiantes, 
+			Set<Usuario> docentes, String observaciones, String tipoAula,
+			boolean asignada, Usuario solicitante, int codigo, char turno, LocalDate fechaInicio,
+			LocalDate fechaFin, String cuatrimestre, String presencialidad) {
+		super(id, fecha, cantEstudiantes, docentes, observaciones, tipoAula, asignada, solicitante);
+		this.codigo = codigo;
+		this.turno = turno;
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+		this.cuatrimestre = cuatrimestre;
+		this.presencialidad = presencialidad;
+	}
+}
