ab39f8d78b3c57294019f1e278e21f7c131a7882
lesrz <suarezarnaldi.leo@gmail.com>
2022-05-20 15:32:25 -0300

    Crear entidad Curso

---
+++ src/main/java/com/webservice/app/entities/Curso.java
@@ -0,0 +1,44 @@
+package com.webservice.app.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+@PrimaryKeyJoinColumn(referencedColumnName= "idNotaPedido", name= "idCurso")
+@Table(name= "curso")
+public class Curso extends NotaPedido {
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name= "codigo")
+	private String codigo;
+	
+	@Column(name= "turno")
+	@NotEmpty()
+	private char turno;
+	
+	@DateTimeFormat(pattern= "yyyy-MM-dd")
+	@Column(name= "fecha_inicio")
+	private LocalDate fechaInicio;
+	
+	@DateTimeFormat(pattern= "yyyy-MM-dd")
+	@Column(name= "fecha_fin")
+	private LocalDate fechaFin;
+	
+	@Column(name= "cuatrimestre")
+	private String cuatrimestre;
+	
+	@Column(name= "presencialidad")
+	private String presencialidad;
+}
