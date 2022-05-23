bc6593cc4e1039fe45ab3c610d8a225e9188fe08
Rocio Roggero <rocio.roggero@hotmail.com.ar>
2022-05-23 15:40:58 -0300

    Agrego entidad y modelo Laboratorio

---
+++ src/main/java/com/webservice/app/models/LaboratorioModel.java
@@ -0,0 +1,37 @@
+package com.webservice.app.models;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import com.webservice.app.entities.Aula;
+import com.webservice.app.entities.Edificio;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class LaboratorioModel extends AulaModel{
+
+	private int cantSillas;
+	private int cantPC;
+	
+	
+	public LaboratorioModel(int numero, Edificio edificio,int cantSillas, int cantPC) {
+		super(numero, edificio);
+		this.cantSillas = cantSillas;
+		this.cantPC = cantPC;
+	}
+	
+	
+
+	public LaboratorioModel(int idAula, int numero, Edificio edificio,int cantSillas, int cantPC) {
+		super(idAula, numero, edificio);
+		this.cantSillas = cantSillas;
+		this.cantPC = cantPC;	
+	}
+
+
+	
+	
+}
