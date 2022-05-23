2a8a08d6e20f2819cea1dfe34a8b859b4b162b26
Rocio Roggero <rocio.roggero@hotmail.com.ar>
2022-05-23 15:37:47 -0300

    Agrego entidad y modelo Aula

---
+++ src/main/java/com/webservice/app/models/AulaModel.java
@@ -0,0 +1,36 @@
+package com.webservice.app.models;
+
+import java.util.Objects;
+
+import com.webservice.app.entities.Edificio;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter @Setter @NoArgsConstructor
+public class AulaModel {
+	
+	private int idAula;
+	private int numero;
+	private Edificio edificio;
+	
+	
+	public AulaModel(int idAula, int numero, Edificio edificio) {
+		this.idAula = idAula;
+		this.numero = numero;
+		this.edificio = edificio;
+	}
+	
+	public AulaModel(int numero, Edificio edificio) {
+		this.numero = numero;
+		this.edificio = edificio;
+	}
+
+
+
+	
+	
+	
+	
+}
