edb93079a5702db6ec868925f76740162c355039
Leonel Suarez Arnaldi <leonel.suarezarnaldi@zerf.com.ar>
2022-05-25 22:01:18 -0300

    Eliminar query redundante

--- src/main/java/com/webservice/app/repositories/INotaPedidoRepository.java
+++ src/main/java/com/webservice/app/repositories/INotaPedidoRepository.java
@@ -1,13 +1,11 @@
 package com.webservice.app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.webservice.app.entities.NotaPedido;
 
 public interface INotaPedidoRepository extends JpaRepository<NotaPedido, Integer>{
 	
-	@Query("FROM nota_pedido np WHERE np.idNotaPedido = (:idNotaPedido)")
 	public NotaPedido findById(int idNotaPedido);
 	
 }
