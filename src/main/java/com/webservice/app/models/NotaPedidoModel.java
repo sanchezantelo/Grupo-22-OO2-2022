0dda15710f8fc7b743606a8695a8b5dd9dbb8e83
Leonel Suarez Arnaldi <leonel.suarezarnaldi@zerf.com.ar>
2022-06-02 00:11:27 -0300

    Incluir materia

--- src/main/java/com/webservice/app/models/NotaPedidoModel.java
+++ src/main/java/com/webservice/app/models/NotaPedidoModel.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.webservice.app.entities.Aula;
+import com.webservice.app.entities.Materia;
 import com.webservice.app.entities.Usuario;
 
 import lombok.NoArgsConstructor;
@@ -21,20 +22,21 @@ public abstract class NotaPedidoModel {
 	protected LocalDate fecha;
 	protected int cantEstudiantes;
 	protected Set<Usuario> docentes;
-	// protected Materia materia;
+	protected Materia materia;
 	protected String observaciones;
 	protected String tipoAula;
 	protected boolean asignada;
 	protected Aula aulaAsignada;
 	protected Usuario solicitante;
 
-	public NotaPedidoModel(int id, LocalDate fecha, int cantEstudiantes, Set<Usuario> docentes, String observaciones,
+	public NotaPedidoModel(int id, LocalDate fecha, int cantEstudiantes, Set<Usuario> docentes, Materia materia, String observaciones,
 			String tipoAula, boolean asignada, Aula aulaAsignada, Usuario solicitante) {
 		super();
 		this.id = id;
 		this.fecha = fecha;
 		this.cantEstudiantes = cantEstudiantes;
 		this.docentes = docentes;
+		this.materia = materia;
 		this.observaciones = observaciones;
 		this.tipoAula = tipoAula;
 		this.asignada = asignada;
@@ -42,12 +44,13 @@ public abstract class NotaPedidoModel {
 		this.solicitante = solicitante;
 	}
 
-	public NotaPedidoModel(LocalDate fecha, int cantEstudiantes, Set<Usuario> docentes, String observaciones,
+	public NotaPedidoModel(LocalDate fecha, int cantEstudiantes, Set<Usuario> docentes, Materia materia, String observaciones,
 			String tipoAula, boolean asignada, Aula aulaAsignada, Usuario solicitante) {
 		super();
 		this.fecha = fecha;
 		this.cantEstudiantes = cantEstudiantes;
 		this.docentes = docentes;
+		this.materia = materia;
 		this.observaciones = observaciones;
 		this.tipoAula = tipoAula;
 		this.asignada = asignada;
