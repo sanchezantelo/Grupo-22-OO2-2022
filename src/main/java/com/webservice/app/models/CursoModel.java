853d58806fc0fd145a97b6b92b22f41524068ec7
Leonel Suarez Arnaldi <leonel.suarezarnaldi@zerf.com.ar>
2022-06-04 16:36:16 -0300

    Cambiar fechas por dia de semana

--- src/main/java/com/webservice/app/models/CursoModel.java
+++ src/main/java/com/webservice/app/models/CursoModel.java
@@ -1,9 +1,5 @@
 package com.webservice.app.models;
 
-import java.time.LocalDate;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -11,11 +7,7 @@ import lombok.NoArgsConstructor;
 @Data @NoArgsConstructor @EqualsAndHashCode(callSuper=false)
 public class CursoModel extends NotaPedidoModel {
 	protected int codigo;
-	protected char turno;
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	protected LocalDate fechaInicio;
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	protected LocalDate fechaFin;
+	protected int diaSemana;
 	protected String cuatrimestre;
 	protected int presencialidad;
 }
