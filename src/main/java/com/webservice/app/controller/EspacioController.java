432d7bba471cc32535a508d3b71b452e6465e8f5
rbroggero <rocio.roggero@hotmail.com.ar>
2022-06-05 17:47:21 -0300

    Agrego funcionalidades Espacio

--- src/main/java/com/webservice/app/controller/EspacioController.java
+++ src/main/java/com/webservice/app/controller/EspacioController.java
@@ -1,14 +1,29 @@
 package com.webservice.app.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
+import com.webservice.app.entities.Aula;
 import com.webservice.app.services.IAulaService;
 import com.webservice.app.services.IEspacioService;
 
 @Controller("espacio")
+@RequestMapping("/espacio" )
 public class EspacioController {
 	
 	@Autowired
@@ -21,5 +36,32 @@ public class EspacioController {
 	private IAulaService aulaService;
 	
 	private ModelMapper modelMapper = new ModelMapper();
+	
+	@GetMapping("/{idAula}")
+	public String traerEspaciosPorAula(@PathVariable("idAula") int idAula, Model model,RedirectAttributes redirectAttrs) {
+		 
+		Aula aula = aulaService.traerAula(idAula);
+		redirectAttrs.addFlashAttribute("aula", aula);
+		redirectAttrs.addFlashAttribute("espacios", espacioService.traerEspacios(aula))
+				.addFlashAttribute("clase", "alert alert-success");
+		return "redirect:/index";
+	}
+	
+	@RequestMapping(value="/add",method = RequestMethod.POST)
+	public String generarEspacios(@RequestParam Map<String, String> parameters,Model model,RedirectAttributes redirectAttrs) 
+	{   
+		
+		LocalDate desde= LocalDate.parse(parameters.get("desde"), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		LocalDate hasta= LocalDate.parse(parameters.get("hasta"), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		try {
+			espacioService.agregarEspacios(desde,hasta);
+			redirectAttrs.addFlashAttribute("success", "Se generaron los espacios").addFlashAttribute("clase", "alert alert-danger");
+		}catch(Exception e) {
+			redirectAttrs.addFlashAttribute("modalGeneradorEspacios", true);
+			redirectAttrs.addFlashAttribute("error", e.getMessage()).addFlashAttribute("clase", "alert alert-danger");
+		}
+
+		return "redirect:/index";
+	}
 
 }
--- src/main/java/com/webservice/app/services/implementation/EspacioService.java
+++ src/main/java/com/webservice/app/services/implementation/EspacioService.java
@@ -1,11 +1,22 @@
 package com.webservice.app.services.implementation;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+
+import com.webservice.app.entities.Aula;
+import com.webservice.app.entities.Espacio;
+import com.webservice.app.helpers.Funciones;
 import com.webservice.app.repositories.IEspacioRepository;
+import com.webservice.app.services.IAulaService;
 import com.webservice.app.services.IEspacioService;
+import com.webservice.app.services.INotaPedidoService;
 
 
 @Service("espacioService")
@@ -15,4 +26,72 @@ public class EspacioService implements IEspacioService{
 	@Qualifier("espacioRepository")
 	private IEspacioRepository espacioRepository;
 	
+	@Autowired
+	@Qualifier("aulaService")
+	private IAulaService aulaService;
+	
+	@Autowired
+	@Qualifier("notaPedidoService")
+	private INotaPedidoService notaPedidoService;
+	
+	private ModelMapper modelMapper = new ModelMapper();
+
+	public List<Espacio> traerEspacios(Aula aula) {
+		return espacioRepository.findByAulaAndActivo(aula,true);
+	}
+	
+	public void agregarEspacios(LocalDate desde,LocalDate hasta) throws Exception{
+		
+		
+		char[] turnos ={ 'M', 'T', 'N'};
+		
+		List<Aula> aulas = aulaService.traerAulas();
+		
+		List<Espacio> espacios = new ArrayList<>();
+		
+		if(aulas.isEmpty()) throw new Exception("No hay aulas cargadas en el sistema");
+		
+		
+		List<Espacio> espaciosActivos = espacioRepository.traerEspaciosActivos();
+
+		
+		while(desde.isBefore(hasta) || desde.isEqual(hasta) ) {
+		
+			for(Aula aula:aulas) {
+				for(char turno:turnos) {
+					if(Funciones.esDiaHabil(desde)) {
+						
+						if(espacioRepository.findByFechaAndTurnoAndAula(desde, turno,aula)!=null) throw new Exception("Ya existe el espacio con fecha "+desde+", turno "+turno+" para el aula "+aula.getNumero()+" del edificio "+aula.getEdificio().getEdificio());
+						Espacio espacio = new Espacio(desde, turno, aula, true,true);
+						espacios.add(espacio);
+
+					}
+				}
+
+			}
+			
+			desde = desde.plusDays(1);
+		}
+		
+		if(this.insertarOActualizar(espacios) ) {
+			if(!espaciosActivos.isEmpty()) this.DesactivarCalendarioAnterior(espaciosActivos);
+		}else {
+			throw new Exception("No se pudo completar la transaccion");
+		}
+
+	}
+
+
+	public boolean insertarOActualizar(Espacio espacio) {
+	    return espacioRepository.save(espacio) != null ? true : false;
+	}
+	
+	public boolean insertarOActualizar(List<Espacio> espacios) {
+	    return espacioRepository.saveAll(espacios) != null ? true : false;
+	}
+	
+	public void DesactivarCalendarioAnterior(List<Espacio> espaciosActivos){
+		espaciosActivos.stream().forEach(espacio -> espacio.setActivo(false));
+		this.insertarOActualizar(espaciosActivos);
+	}
 }
